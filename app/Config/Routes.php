eb1bc2de28c5ae26b3ef42976eaee015aebf80d4
emmanuel campait <ecampait@gmail.com>
2023-09-14 16:29:32 +0200

    Use supported locales only

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+// Locale support
+$routes->useSupportedLocalesOnly(true);
+
 // Frontend
 $routes->get('/', 'Frontend\Home::index');
 $routes->get('/{locale}', 'Frontend\Home::index');
