782b50ddeb6f63d149ec9b48ec2eb3b9a66af05c
emmanuel campait <ecampait@gmail.com>
2023-09-14 16:28:17 +0200

    Added a language management file for the Frontend
    en & fr only...

---
+++ app/Language/en/AppFrontend.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) domProjects <admin@domprojects.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Frontend language settings
+return [
+    'copyright' => "&copy; %s %s - All rights reserved",
+    'menu' => [
+        'administration' => "Administration",
+        'home'           => "Home",
+    ],
+    'menuAria' => [
+        'toggleNavigation' => "Toggle navigation",
+    ],
+];
---
+++ app/Language/fr/AppFrontend.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) domProjects <admin@domprojects.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Frontend language settings
+return [
+    'copyright' => "&copy; %s %s - Tous droits réservés",
+    'menu' => [
+        'administration' => "Administration",
+        'home'           => "Accueil",
+    ],
+    'menuAria' => [
+        'toggleNavigation' => "Basculer la navigation",
+    ],
+];
